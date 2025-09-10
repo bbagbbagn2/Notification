71c3ee93730b0a9df417205d453567297ac64efe
bbagbbagn2 <pyoungh999@naver.com>
2025-09-10 10:32:31 +0900

    feat: 문서 생성하는 버튼 추가

--- src/app/_components/PageHeader.tsx
+++ src/app/_components/PageHeader.tsx
@@ -5,21 +5,33 @@ import colors from '../../styles/theme';
 
 export default function PageHeader() {
   return (
-    <Wrapper>
-      <p>공지사항</p>
-    </Wrapper>
+    <PageHeaderContainer>
+      <PageHeaderTitle>All docs</PageHeaderTitle>
+      <ButtonContainer>Start a new document</ButtonContainer>
+    </PageHeaderContainer>
   );
 }
 
-const Wrapper = styled.div`
-  margin-top: 60px;
+const PageHeaderContainer = styled.div`
+  margin-top: 32px;
+  margin-bottom: 50px;
   height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-  & > p {
-    color: ${colors.text};
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 100%;
-    letter-spacing: -0.08px;
-  }
+const PageHeaderTitle = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 100%;
+  color: ${colors.text};
+`;
+
+const ButtonContainer = styled.button`
+  padding: 8px 12px;
+  background-color: ${colors.primary300};
+  color: ${colors.white};
+  border-radius: 4px;
+  border: 1px solid ${colors.primary};
 `;
